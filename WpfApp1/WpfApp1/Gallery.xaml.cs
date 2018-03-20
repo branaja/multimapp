afee3e206a10f02f22cb5fd80e5d788231ec5104
branaja <branimir.jungic@gmail.com>
2018-03-20 23:26:37 +0100

    Popravljen dizajn početng ekrana (još bi trebalo promijeniti naslov i ikonu). Dodana galerija (novi prozor), radi brisanje na klik. Problem - zip je već nego originalni fileovi :/ Ako se konvertira u jpg sa opcijom 100L (dakle bez gubitka kvalitete) onda je zip višestruko veći, sa 50L je manji.

---
+++ WpfApp1/WpfApp1/Gallery.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for Gallery.xaml
+    /// </summary>
+    public partial class Gallery : Window
+    {
+        private List<string> paths;
+        private int count = 0;
+        private MainWindow parent;
+        public Gallery()
+        {
+            InitializeComponent();
+        }
+
+        public Gallery(List<string> paths, MainWindow parent)
+        {
+            InitializeComponent();
+            this.paths = paths;
+            this.parent = parent;
+            count = 0;
+            foreach(string img in paths)
+            {
+                addImg2Grid(img);
+            }
+        }
+
+        private void addImg2Grid(string filename)
+        {
+            try
+            {
+                if (count / 10 + 1 > gridImg.RowDefinitions.Count())
+                    gridImg.RowDefinitions.Add(new RowDefinition());
+                if (gridImg.ColumnDefinitions.Count() < 10)
+                    gridImg.ColumnDefinitions.Add(new ColumnDefinition());
+
+                BitmapImage image = new BitmapImage();
+                image.BeginInit();
+                image.CacheOption = BitmapCacheOption.OnLoad;
+                image.UriSource = new Uri(filename);
+                image.DecodePixelWidth = 1000;
+                image.EndInit();
+
+               
+                Image img = new Image();
+
+                Grid.SetColumn(img, count % 10);
+                Grid.SetRow(img, count / 10);
+
+                img.Source = image;
+                img.MouseEnter += removePreview;
+                img.MouseLeave += removeNoMore;
+                img.MouseDown += deleteSelected;
+                gridImg.Children.Add(img);
+                count++;
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+            
+        }
+        private void removePreview(object sender, MouseEventArgs e)
+        {
+            Image img = (Image)sender;
+            if (img.Opacity != 0)
+            {
+                img.Opacity = 0.2;
+                img.Cursor = Cursors.No;
+            }
+        }
+
+        private void removeNoMore(object sender, MouseEventArgs e)
+        {
+            Image img = (Image)sender;
+            if(img.Opacity!=0) img.Opacity = 1;
+        }
+
+        private void deleteSelected(object sender, MouseEventArgs e)
+        {
+            Image img = (Image)sender;
+            int index = gridImg.Children.IndexOf(img);
+            img.Opacity = 0;
+            parent.deleteImage(index);
+            gridImg.Children.RemoveAt(index);
+        }
+    }
+}
--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SevenZip;
+using System;
 using System.Collections.Generic;
 using System.Drawing.Imaging;
 using System.Linq;
@@ -13,6 +14,15 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Google.Apis.Drive.v2;
+using Google.Apis.Auth.OAuth2;
+using System.Threading;
+using Google.Apis.Util.Store;
+using Google.Apis.Services;
+using Google.Apis.Drive.v2.Data;
+using System.Security.Cryptography.X509Certificates;
+using System.IO;
+using System.Reflection;
 
 namespace WpfApp1
 {
@@ -26,14 +36,14 @@ namespace WpfApp1
         public MainWindow()
         {
             InitializeComponent();
-            textBox1.Text = "";
             listaPathova = new List<string>();
             savePath = "";
-
+            doneImg.Visibility = Visibility.Hidden;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            doneImg.Visibility = Visibility.Hidden;
             Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
 
             //dlg.DefaultExt = ".png";
@@ -42,67 +52,27 @@ namespace WpfApp1
             dlg.Multiselect = true;
 
             Nullable<bool> result = dlg.ShowDialog();
-            int count = 0;
+           
             
             //dodavanje slika u grid bi možda trebalo u zasebnoj dretvi ili nešta napraviti koja bi
             //dodavala slike u realnom vremenu?
             if (result == true)
             {
-                textBox1.Text = "";
-                imgGrid.Children.Clear();
                 listaPathova.Clear();
                 // Open document 
                 if (dlg.FileNames != null && dlg.FileNames.Count() > 0)
                 {
                     savePath = System.IO.Path.GetDirectoryName(dlg.FileNames[0]);
-                    outputPath.Text = savePath;
                 }
                 foreach (string filename in dlg.FileNames)
                 {
-                    textBox1.Text += filename + "\n";
                     listaPathova.Add(filename);
-                    try
-                    {
-                        if (count / 5 + 1 > imgGrid.RowDefinitions.Count())
-                            imgGrid.RowDefinitions.Add(new RowDefinition());
-                        if(imgGrid.ColumnDefinitions.Count() <5)
-                            imgGrid.ColumnDefinitions.Add(new ColumnDefinition());
-
-                        Image img = new Image();
-                        Grid.SetColumn(img, count%5);
-                        Grid.SetRow(img, count/5);
-                        img.Source = new BitmapImage(new Uri(filename));
-                        img.MouseEnter += removePreview;
-                        img.MouseLeave += removeNoMore;
-                        imgGrid.Children.Add(img);
-                        count++;
-                        
-                    }
-                    catch (Exception ex)
-                    {
-
-                    }
-
-                    }
+                }
+                noImg.Text = "Odabrano je " + listaPathova.Count() + " slika.";
             }
         }
 
-        private void removePreview(object sender, MouseEventArgs e)
-        {
-            Image img = (Image)sender;
-            img.Opacity = 0.2;
-            img.Cursor = Cursors.No;
-            Label lbl = new Label();
-            lbl.Content = "X";
-            lbl.Foreground = new SolidColorBrush(Colors.Red);
-            
-        }
-
-        private void removeNoMore(object sender, MouseEventArgs e)
-        {
-            Image img = (Image)sender;
-            img.Opacity = 1;
-        }
+        
 
         private void convertBttn_Click(object sender, RoutedEventArgs e)
         {
@@ -110,7 +80,6 @@ namespace WpfApp1
             var result = dlg.ShowDialog();
             if (result == System.Windows.Forms.DialogResult.OK)
             {
-                outputPath.Text = dlg.SelectedPath;
                 savePath = dlg.SelectedPath;
             }
         }
@@ -119,31 +88,35 @@ namespace WpfApp1
         {
             foreach(string img in listaPathova)
             {
-                System.Drawing.Bitmap bmp1 = new System.Drawing.Bitmap(img);
-                ImageCodecInfo jgpEncoder = GetEncoder(ImageFormat.Jpeg);
-
-                // Create an Encoder object based on the GUID
-                // for the Quality parameter category.
-                System.Drawing.Imaging.Encoder myEncoder =
-                    System.Drawing.Imaging.Encoder.Quality;
-
-                // Create an EncoderParameters object.
-                // An EncoderParameters object has an array of EncoderParameter
-                // objects. In this case, there is only one
-                // EncoderParameter object in the array.
-                EncoderParameters myEncoderParameters = new EncoderParameters(1);
-
-                EncoderParameter myEncoderParameter = new EncoderParameter(myEncoder,50L);
-                myEncoderParameters.Param[0] = myEncoderParameter;
-                string temp = savePath +"\\"+ System.IO.Path.GetFileName(img);
-                bmp1.Save(savePath + "\\" + System.IO.Path.GetFileName(img), jgpEncoder,
-                    myEncoderParameters);
+                //convert2jpg(img);
 
 
                 //update progressbara ne radi u realnom vremenu :( 
                 int i = (listaPathova.IndexOf(img) + 1) / listaPathova.Count() * 100;
-                Dispatcher.Invoke(() => { this.pBar.Value = i; });
+                //Dispatcher.Invoke(() => { this.pBar.Value = i; });
             }
+        }
+        private void convert2jpg(string img, string savePath)
+        {
+            System.Drawing.Bitmap bmp1 = new System.Drawing.Bitmap(img);
+            ImageCodecInfo jgpEncoder = GetEncoder(ImageFormat.Jpeg);
+
+            // Create an Encoder object based on the GUID
+            // for the Quality parameter category.
+            System.Drawing.Imaging.Encoder myEncoder =
+                System.Drawing.Imaging.Encoder.Quality;
+
+            // Create an EncoderParameters object.
+            // An EncoderParameters object has an array of EncoderParameter
+            // objects. In this case, there is only one
+            // EncoderParameter object in the array.
+            EncoderParameters myEncoderParameters = new EncoderParameters(1);
+
+            EncoderParameter myEncoderParameter = new EncoderParameter(myEncoder, 50L);
+            myEncoderParameters.Param[0] = myEncoderParameter;
+            string temp = savePath + "\\" + System.IO.Path.GetFileNameWithoutExtension(img) + ".jpg";
+            bmp1.Save(temp, jgpEncoder,
+                myEncoderParameters);
         }
         private ImageCodecInfo GetEncoder(ImageFormat format)
         {
@@ -159,6 +132,157 @@ namespace WpfApp1
             }
             return null;
         }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            compress(savePath);
+        }
+
+        private void compress(string savePath)
+        {
+            var temp = new SevenZipCompressor();
+            temp.ScanOnlyWritable = true;
+            string savename = "";
+            if (zipName.Text.Contains(".7z"))
+                savename = zipName.Text;
+            else
+                savename = zipName.Text + ".7z";
+            temp.CompressDirectory(savePath + "\\tempfolder", savePath + "\\" + savename);
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            System.IO.Directory.CreateDirectory(savePath+"\\tempfolder");
+            if(chkBox.IsChecked == true)
+            {
+                foreach(string img in listaPathova)
+                {
+                    convert2jpg(img, savePath + "\\tempfolder");
+                }
+            } else
+            {
+                foreach(string img in listaPathova)
+                {
+                    System.IO.File.Copy(img, savePath + "\\tempfolder\\" + System.IO.Path.GetFileName(img));
+                }
+            }
+
+            compress(savePath);
+
+            System.IO.Directory.Delete(savePath + "\\tempfolder", true);
+
+            doneImg.Visibility = Visibility.Visible;
+        }
+
+        private void galleryBttn_Click(object sender, RoutedEventArgs e)
+        {
+            Gallery gallery = new Gallery(listaPathova, this);
+            gallery.Show();
+        }
+
+        public void deleteImage(int index)
+        {
+            listaPathova.RemoveAt(index);
+            noImg.Text = "Odabrano je " + listaPathova.Count() + " slika.";
+        }
+
+        /*
+        private void auth()
+        {
+            string[] scopes = new string[] { DriveService.Scope.Drive }; // Full access
+
+            var keyFilePath = @"c:\file.p12";    // Downloaded from https://console.developers.google.com
+            var serviceAccountEmail = "branimir.jungic@gmail.com";  // found https://console.developers.google.com
+
+            //loading the Key file
+            var certificate = new X509Certificate2(keyFilePath, "notasecret", X509KeyStorageFlags.Exportable);
+            var credential = new ServiceAccountCredential(new ServiceAccountCredential.Initializer(serviceAccountEmail)
+            {
+                Scopes = scopes
+            }.FromCertificate(certificate));
+
+            var service = new DriveService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = "Drive API Sample",
+            });
+
+
+        }
+
+        public static File createDirectory(DriveService _service, string _title, string _description, string _parent)
+        {
+
+            File NewDirectory = null;
+
+            // Create metaData for a new Directory
+            File body = new File();
+            body.Title = _title;
+            body.Description = _description;
+            body.MimeType = "application/vnd.google-apps.folder";
+            body.Parents = new List() { new ParentReference() { Id = _parent } };
+            try
+            {
+                FilesResource.InsertRequest request = _service.Files.Insert(body);
+                NewDirectory = request.Execute();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An error occurred: " + e.Message);
+            }
+
+            return NewDirectory;
+        }
+
+        // tries to figure out the mime type of the file.
+        private static string GetMimeType(string fileName)
+        {
+            string mimeType = "application/unknown";
+            string ext = System.IO.Path.GetExtension(fileName).ToLower();
+            Microsoft.Win32.RegistryKey regKey = Microsoft.Win32.Registry.ClassesRoot.OpenSubKey(ext);
+            if (regKey != null && regKey.GetValue("Content Type") != null)
+                mimeType = regKey.GetValue("Content Type").ToString();
+            return mimeType;
+        }
+
+        public static File uploadFile(DriveService _service, string _uploadFile, string _parent) {
+            
+            if (System.IO.File.Exists(_uploadFile))
+            {
+                File body = new File();
+                body.Title = System.IO.Path.GetFileName(_uploadFile);
+                body.Description = "File uploaded by Diamto Drive Sample";
+                body.MimeType = GetMimeType(_uploadFile);
+                body.Parents = new List() { new ParentReference() { Id = _parent } };
+
+                // File's content.
+                byte[] byteArray = System.IO.File.ReadAllBytes(_uploadFile);
+                System.IO.MemoryStream stream = new System.IO.MemoryStream(byteArray);
+                try
+                {
+                    FilesResource.InsertMediaUpload request = _service.Files.Insert(body, stream, GetMimeType(_uploadFile));
+                    request.Upload();
+                    return request.ResponseBody;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("An error occurred: " + e.Message);
+                    return null;
+                }
+            }
+            else {
+                Console.WriteLine("File does not exist: " + _uploadFile);
+                return null;
+            }           
+        
+        }
+        */
+
     }
 
    
